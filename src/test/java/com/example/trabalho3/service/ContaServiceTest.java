a51ba251219668bb2c12a38bd3d48a10e28976b0
Raphael <raphamelo41@gmail.com>
2025-12-10 21:19:02 -0300

    testes contaService

--- src/test/java/com/example/trabalho3/service/ContaServiceTest.java
+++ src/test/java/com/example/trabalho3/service/ContaServiceTest.java
@@ -1,4 +1,25 @@
 package com.example.trabalho3.service;
 
+import com.example.trabalho3.repository.ContaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class ContaServiceTest {
+
+    @Mock
+    private ContaRepository contaRepository;
+
+    @InjectMocks
+    private ContaService contaService;
+
+    @BeforeEach
+    public void setUp(){ MockitoAnnotations.initMocks(this); }
+
+
+    @Test
+    void testaAdicao(){
+    }
 }
